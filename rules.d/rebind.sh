ca87ab8b5ac169fad4e0c846e82098a7a3fc4ba1
slawkis <sszczyrba@gmail.com>
2016-12-09 11:14:24 +0100

    Add files via upload

--- rules.d/rebind.sh
+++ rules.d/rebind.sh
@@ -11,11 +11,13 @@ if [ "x$DEV" == "x" ]; then logger "rebind: no device"; exit; fi
 logger "rebind: device = $DEV"
 
 declare -a PARTS
-#PARTS=( $( echo $DEV | while read -d '/' DPATH ; do echo $DPATH ; done ) )	#index=5
 readarray -n 0 -t -d '/' PARTS <<< $(echo $DEV)					#index=6
 
 read DEV <<< $( echo ${PARTS[6]} )  # no extra \n chars...
 if [ "x$DEV" == "x" ]; then logger "rebind: no device"; exit; fi
+
+NUM=${DEV:${#DEV}-1}
+if [ $NUM -ne 0 ]; then exit; fi
 
 if [   -L "$HIDPATH/$DEV" ]; then logger "rebind: trying to unbind $DEV from HID"; echo $DEV > $HIDPATH/unbind ; sleep 1; fi
 if [ ! -L "$USBPATH/$DEV" ]; then logger "rebind: trying to bind $DEV to XPPEN"  ; echo $DEV > $USBPATH/bind   ; sleep 1; fi
